b0ae52cfea403d32defa245f24d750079856e62e
Mfalme Griffin <mfalmegriffin@gmail.com>
2026-03-07 14:46:52 +0300

    update

--- src/app/api/auth/link-guest.ts
+++ src/app/api/auth/link-guest.ts
@@ -1,5 +1,5 @@
 import { NextResponse } from "next/server";
 
 export async function POST(req: Request) {
-  return NextResponse.json({ message: "Link Guests API" });
+  return NextResponse.json({ message: "Link Guests API "+req });
 }
